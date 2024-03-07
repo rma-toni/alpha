7818185eb1756dd0522699018b2ee24fa33285c6
rma-toni <rmaflores.sw@gmail.com>
2024-03-06 22:18:57 -0300

    Inicio del proyecto

---
+++ SLManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SLManager : MonoBehaviour
+{
+    public void Save()
+    {
+        Debug.Log("Hello im saving data :D");
+    }
+}
