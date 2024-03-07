e6c26700fafe06509ea69bb8332f218e2e0216e3
rma-toni <rmaflores.sw@gmail.com>
2024-03-06 22:48:32 -0300

    Starting the construction of a Serealized save/laod data system

--- GameManager.cs
+++ GameManager.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-
 {
+    int money;
 
     public SLManager sl;
     // Start is called before the first frame update
     void Start()
     {
-        sl.Save();
+        sl.Load();
     }
 
     // Update is called once per frame
--- SLManager.cs
+++ SLManager.cs
@@ -8,4 +8,9 @@ public class SLManager : MonoBehaviour
     {
         Debug.Log("Hello im saving data :D");
     }
+
+    public void Load()
+    {
+        Debug.Log("Hello im loading data :D");
+    }
 }
