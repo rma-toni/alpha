29194b41082caed960ad7c1f0f0f0c88fc35d3f4
rma-toni <rmaflores.sw@gmail.com>
2024-03-07 02:07:33 -0300

    Se termino el save and load system y un sistema de movimiento basico, ahora se procede a realizar el control de camara y rotacion de personaje con la misma

--- GameManager.cs
+++ GameManager.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    
     int money;
+    public Text moneyText;
+
+    //DEBUG
+    public Button moneyButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        SLManager.Load();
+        money = SLManager.Load();
+        moneyText.text = money.ToString();
+        moneyButton.onClick.AddListener(click);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void click()
+    {
+        Debug.Log("Ay me presionaron");
+        money++;
+        SLManager.Save(money);
+        moneyText.text = money.ToString();
     }
 }
--- PlayerController.cs
+++ PlayerController.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float movementSpeed = 5.0f;
+    private float sensitivity = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+           
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerMovement();
+        cameraMovement();
+    }
+
+    void playerMovement()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position = transform.position + new Vector3(0,0,movementSpeed*Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position = transform.position + new Vector3(0,0,-movementSpeed*Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position = transform.position + new Vector3(-movementSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position = transform.position + new Vector3(movementSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void cameraMovement()
+    {
+        if (Input.GetKey("r"))
+        {
+            Debug.Log("Se presiono la tecla de rotacion");
+        }
     }
 }
