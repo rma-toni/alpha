573cbc4b88f65398a4c9f5aba6c8f0b404e54e24
rma-toni <rmaflores.sw@gmail.com>
2024-03-08 14:21:51 -0300

    Se agrego un menu principal el cual carga la escena del juego, hayque corregir un error de la camara que tiene margen de salirse de los valores aceptables

--- CameraController.cs
+++ CameraController.cs
@@ -19,18 +19,18 @@ public class CameraController : MonoBehaviour
     void cameraMovement()
     {
         float MouseY = Input.GetAxis("Mouse Y");
-        if (transform.rotation.x >= -0.15f && transform.rotation.x < 0.075f)
+        if (transform.rotation.x >= -0.1f && transform.rotation.x < 0.1f)
         {
             Debug.Log("En if principal");
             transform.Rotate(new Vector3(-MouseY, 0f, 0f));
-        } else if (transform.rotation.x < -0.15f)
+        } else if (transform.rotation.x < -0.1f)
         {
             Debug.Log("En if arriba");
             if (MouseY < 0)
             {
                 transform.Rotate(new Vector3(-MouseY, 0f, 0f));
             }
-        } else if (transform.rotation.x > 0.075f)
+        } else if (transform.rotation.x > 0.1f)
         {
             Debug.Log("En if abajo");
             if (MouseY > 0) {
